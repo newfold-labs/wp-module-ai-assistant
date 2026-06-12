98d4012e0ad72f93b8e85dfb069f6118782b1c73
Abhijit Bhatnagar <abhijit.bhatnagar@gmail.com>
2026-06-12 13:15:41 +0530

    added product posttype for indexing

--- includes/Services/KnowledgeStore.php
+++ includes/Services/KnowledgeStore.php
@@ -271,7 +271,7 @@ class KnowledgeStore {
 	 * @return array<int, string>
 	 */
 	public static function indexable_post_types() {
-		$types = array( 'page', 'post' );
+		$types = array( 'page', 'post', 'product' );
 		return apply_filters( 'nfd_ai_assistant_indexable_post_types', $types );
 	}
 }
